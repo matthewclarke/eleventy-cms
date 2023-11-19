919ca662ba8151ea3f60b90d120bd75077c1e153
Matthew Clarke <matthewclarke@gmail.com>
2023-11-18 22:49:14 -0800

    get data from githun

--- src/_data/db.js
+++ src/_data/db.js
@@ -1,8 +1,9 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function() {
-  let url ='localhost:9000/src/api/data.json';
-//https://raw.githubusercontent.com/ProjectSakura/OTA/10/changelog/changelog_beryllium.txt
+  //let url ='localhost:9000/src/api/data.json';
+// const url = 'https://raw.githubusercontent.com/eleventy-cms/blob/production/src/_data/db.js;
+ const url = 'https://raw.githubusercontent.com/matthewclarke/eleventy-cms/master/src/api/data.json';
 
   /* This returns a promise */
   return EleventyFetch(url, {
